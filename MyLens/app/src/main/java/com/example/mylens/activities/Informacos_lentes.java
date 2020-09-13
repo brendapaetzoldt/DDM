77e8af600c67aac922e48983620dbbf384616fd4
brendapaetzoldt <brendapaetzoldt@yahoo.com.br>
2020-09-13 14:48:22 -0300

    Exclusão do fragment de info lentes e inclusão da activity

---
+++ MyLens/app/src/main/java/com/example/mylens/activities/Informacos_lentes.java
@@ -0,0 +1,16 @@
+package com.example.mylens.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mylens.R;
+
+public class Informacos_lentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacos_lentes);
+    }
+}
